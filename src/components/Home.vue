09bb62f7da1b0b0d2e1e9d4897fab2a49921e48f
HyunWoo Jeon <medianhw@gmail.com>
2017-08-06 23:07:40 +0900

    Home 화면에서의 로그인, 회원가입 창을 Vuex를 이용하여 Store 생성 및 이벤트 바인딩 완료

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,37 @@
 <template lang="pug"> 
   .home-container
     header-logo
-    sign-in
+    sign-in(
+      v-show='signIn'
+    )
+    sign-up(
+      v-show='signUp'
+    )
     video-bg
-    .home-slogan.susy-home
+    .home-slogan.susy-home(
+      v-show='mainView'
+    )
       main.home-slogan-first
         p Love, Eat, and Write Your Diary
       main.home-slogan-second
         | 오늘 먹었던 맛있는 음식을 남기는 나만의 공간. 
         br
         | 자신만의 맛있는 일기장을 만들어 보세요!  
-      .buttons
-        button.buttons-signup 회원가입
-        button.buttons-signin 로그인
+      .home-buttons
+        button.home-buttons-signup(
+          type='button' 
+          @click='openSignUp') 회원가입
+        button.home-buttons-signin(
+          type='button'
+          @click='openSignIn') 로그인
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
 import VideoBg from './Video'
 import HeaderLogo from './Header'
 import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 export default {
   name: 'Home',
@@ -26,10 +39,26 @@ export default {
     VideoBg,
     HeaderLogo,
     SignIn,
+    SignUp
   },
   data() {
     return {
 
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'signUp',
+      'signIn',
+      'mainView'
+    ])
+  },
+  methods: {
+    openSignUp() {
+      this.$store.commit('openSignUp')
+    },
+    openSignIn() {
+      this.$store.commit('openSignIn') 
     }
   }
 }
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,40 +1,56 @@
 <template lang="pug">
   .signin-container.susy-user
-    .user-content-border
-      main.user-content
-        button.user-delete X
-        .user-buttons
-          button.user-signin 로그인
-          button.user-signup 회원가입
-        .user-input-box
-          .user-input
-            form
-              legend 로그인 및 회원가입 폼
-              fieldset
-                label(for='email')
-                  input#email(type='email', value='', placeholder='이메일을 입력해 주세요')
-                label(for='password')
-                  input#password(type='password', value='', placeholder='비밀번호를 6자 이상 입력해 주세요.')
-            .user-buttons-signin
-              button.user-login
-                span 로그인
-              button.user-facebook
-                span 페이스북 아이디로 로그인
-            hr
-            p.notice-facebook Facebook아이디로 간편하게 로그인 할 수 있습니다.
+    main.signin-content-wrapper
+      .signin-content-wrapper-inside
+        button.signin-content-wrapper-inside-delete(
+          type='button'
+          @click='closeSignIn') X
+        .signin-content-buttons
+          button.signin-content-buttons-signin(type='button') 로그인
+          button.signin-content-buttons-signup(
+            type='button'
+            @click='openSignUp') 회원가입
+        .signin-input
+          form
+            legend 로그인 및 회원가입 폼
+            fieldset
+              label(for='email')
+                input#email(type='email', value='', placeholder='이메일을 입력해 주세요')
+              label(for='password')
+                input#password(type='password', value='', placeholder='비밀번호를 6자 이상 입력해 주세요.')
+          .signin-buttons-signin
+            button.signin-login(type='button')
+              span 로그인
+            button.signin-facebook(type='button')
+              span 페이스북 아이디로 로그인
+          hr
+          p.signin-notice-facebook Facebook아이디로 간편하게 로그인 할 수 있습니다.
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
   name: 'SignIn',
   data() {
     return{
 
     }
+  },
+  computed: {
+    ...mapGetters([
+      'signUp',
+      'signIn',
+      'mainView'
+    ])
+  },
+  methods: {
+    openSignUp() {
+      this.$store.commit('openSignUp')
+    },
+    closeSignIn() {
+      this.$store.commit('closeSignIn')
+    }
   }
 }
 </script>
-
-<style lang="sass">
-
-</style>
