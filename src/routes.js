e27b8e5dd055549bc73e72a40b7b08d621704952
soyeon1128 <ksy4334334@gmail.com>
2017-08-08 20:35:43 +0900

    컴포넌트 경로 수정 및 라우트 수정

--- src/routes.js
+++ src/routes.js
@@ -10,14 +10,19 @@ export const routes = [
   {
     path: '/',
     name: 'Home',
-    beforeEnter(to, from, next){
-      let tk = window.localStorage.getItem('token');
-      if (!tk) {
-        next('/calendar');
-      } else {
-        next();
-      }
-    },
+    
+    // Navigation Guard 사용시 필요 코드
+    // 토큰값이 있으면(로그인에 성공하면) Calendar 뷰를 보여줌
+
+    // beforeEnter(to, from, next){
+    //   let tk = window.localStorage.getItem('token');
+    //   if (tk) {
+    //     next('/calendar');
+    //   } else {
+    //     next();
+    //   }
+    // },
+    
     components: {
       default: Home
     }
