4c8fad998d52bd5506d25896765c1d8641a93af6
soyeon1128 <ksy4334334@gmail.com>
2017-08-08 17:50:23 +0900

    캘린더 이전달/다음달 동적 구현 및 주석 추가

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -3,12 +3,12 @@
     header-logo
     .calendar.susy-main
       .cal-header
-        button.btn-prev-month
-          | <
-        span.current-month-header
-          | Aug 2017
-        button.btn-next-month
-          | >
+        span.current-cal-header(v-text="calHeader")        
+        .cal-header-btn-group
+          button.btn-prev-month(@click="onPrevCalendar")
+            | <
+          button.btn-next-month(@click="onNextCalendar")
+            | >
       .cal-table
         table
           colgroup
@@ -35,57 +35,85 @@ export default {
   components: {
     HeaderLogo
   },
-  created () {
+  created() {
     this.makeCalendar();
   },
-  data () {
+  data() {
     return {
       currentMonth: new Date(),
+      calHeader: '',
       arrDay: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
-      arrWeek: ["1st-week", "2nd-week", "3rd-week", "4th-week", "5th-week", "6th-week"],
       arrIsThisMonth : [],
       arrTargetDate: [],
     }
   },
-  computed: {
-    makeShortDate() {
-      
-    }
-  },
   methods: {
+    // 매달의 달력 생성 함수
     makeCalendar() {
+      // 기준 일
+      let date = this.currentMonth;
+      let arrMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-      this.currentMonth.setDate(1);
+      // 달력 헤더 (현재 월, 년)
+      this.calHeader = arrMonth[date.getMonth()] + ' ' + date.getFullYear();
 
-      let targetDate = new Date(this.currentMonth.getTime());
+      // 기준 월의 1일로 초기화
+      date.setDate(1);
+
+      // 달력의 첫 칸에 들어가는 날짜
+      // cf. 2017년 8월의 달력의 경우, 7월 30일
+      // 기준 월의 1일 날짜에서 1일의 요일 index를 빼준다
+      let targetDate = new Date(date.getTime());
       targetDate.setDate(targetDate.getDate() - targetDate.getDay());
 
-      for(let i=0; i<100; i++) {
-        
-        let isThisMonth = "";
+      // 데이터 속성 중 arrIsThisMonth, arrTargetDate 초기화
+      this.arrIsThisMonth = [];
+      this.arrTargetDate = [];
 
-        if (this.currentMonth.getMonth() != targetDate.getMonth()) {
-          isThisMonth = 'not-this-month'
+      // 달력 셀에 삽입되는 targetDate와 클래스 지정 반복문
+      // 달력 셀의 max값은 42(6*7)
+      for(let i=0; i<42; i++) {
+        // 기준 월 날짜 여부에 따른 클래스 지정
+        // 이전달, 다음달 : not-this-month / 이번달: this-month
+        let isThisMonth = "";
+        if (date.getMonth() !== targetDate.getMonth()) {
+          isThisMonth = 'not-this-month';
         } else {
-          isThisMonth = 'this-month'
+          isThisMonth = 'this-month';
         };
 
+        // 기준 월 여부 클래스를 배열 arrIsThisMonth에 푸시()
         this.arrIsThisMonth.push(isThisMonth);
 
+        // 달력 셀에 삽입되는 날짜를 배열 arrTargetDate에 푸시
         this.arrTargetDate.push(new Date(targetDate).getDate());
 
+        // 다음 날 계산 (+1일)
         targetDate.setDate(targetDate.getDate() + 1);
 
+        // 달력 종료
+        // 토요일이면서 기준월이 아닐 경우 반복문 종료
         if(i % 7 ==6) {
-          if(this.currentMonth.getMonth() != targetDate.getMonth()) {
+          if(date.getMonth() != targetDate.getMonth()) {
             break;
           }
         }
-      }  
-    }
+      }
+    },
+    // 이전달 달력 생성 함수
+    onPrevCalendar() {
+      let date = this.currentMonth;
+      date.setMonth(date.getMonth() - 1);
+      this.makeCalendar();
+    },
+    // 다음달 달력 생성 함수
+    onNextCalendar() {
+      let date = this.currentMonth;
+      date.setMonth(date.getMonth() + 1);
+      this.makeCalendar();
+    },
   }
 }
-
 </script>
 
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,5 @@
 <template lang="pug"> 
-  .home-container
-    header-logo
-    sign-in( v-show='signIn' )
-    sign-up( v-show='signUp' )
-    video-bg
-    .home-slogan.susy-home( v-show='mainView' )
-      main.home-slogan-first
-        p Love, Eat, and Write Your Diary
-      main.home-slogan-second
-        | 오늘 먹었던 맛있는 음식을 남기는 나만의 공간. 
-        br
-        | 자신만의 맛있는 일기장을 만들어 보세요!  
-      .home-buttons
-        button.home-buttons-signup(
-          type='button' 
-          @click='openSignUp') 회원가입
-        button.home-buttons-signin(
-          type='button'
-          @click='openSignIn') 로그인
+  calendar
 </template>
 
 <script>
@@ -27,6 +9,7 @@ import VideoBg from './Video'
 import HeaderLogo from './Header'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
+import Calendar from './Calendar/Calendar'
 
 export default {
   name: 'Home',
@@ -35,6 +18,7 @@ export default {
     HeaderLogo,
     SignIn,
     SignUp,
+    Calendar,
   },
   data() {
     return {
