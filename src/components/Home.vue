e3bf63b789cbedbc0fa4fdddc8c3adb1b948a1f6
soyeon1128 <ksy4334334@gmail.com>
2017-08-10 21:17:36 +0900

    비디오 소스 업로드 및 파일 정리

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,23 @@
 <template lang="pug">
-  post
-  //- .home-container
-  //-   header-logo
-  //-   sign-in( v-show='signIn' )
-  //-   sign-up( v-show='signUp' )
-  //-   video-bg
-  //-   .home-slogan.susy-home( v-show='mainView' )
-  //-     main.home-slogan-first
-  //-       p Love, Eat, and Write Your Diary
-  //-     main.home-slogan-second
-  //-       | 오늘 먹었던 맛있는 음식을 남기는 나만의 공간. 
-  //-       br
-  //-       | 자신만의 맛있는 일기장을 만들어 보세요!  
-  //-     .home-buttons
-  //-       button.home-buttons-signup(
-  //-         type='button' 
-  //-         @click='openSignUp') 회원가입
-  //-       button.home-buttons-signin(
-  //-         type='button'
-  //-         @click='openSignIn') 로그인
+  .home-container
+    header-logo
+    sign-in( v-show='signIn' )
+    sign-up( v-show='signUp' )
+    video-bg
+    .home-slogan.susy-home( v-show='mainView' )
+      main.home-slogan-first
+        p Love, Eat, and Write Your Diary
+      main.home-slogan-second
+        | 오늘 먹었던 맛있는 음식을 남기는 나만의 공간. 
+        br
+        | 자신만의 맛있는 일기장을 만들어 보세요!  
+      .home-buttons
+        button.home-buttons-signup(
+          type='button' 
+          @click='openSignUp') 회원가입
+        button.home-buttons-signin(
+          type='button'
+          @click='openSignIn') 로그인
 </template>
 
 <script>
